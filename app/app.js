6f6d731ea9f00d3b128b81e4d27e7a215bfbe9ce
Max Dobler <mdblr@outlook.com>
2016-07-31 15:45:44 -0700

    fix indentation

--- app/app.js
+++ app/app.js
@@ -1,21 +1,22 @@
 'use strict';
 (() => {
   angular
-  .module('demo',['ngRoute'])
-  .config(config);
+    .module('demo',['ngRoute'])
+    .config(config);
 
-  config.$inject = ['$routeProvider','$locationProvider'];
+    config.$inject = ['$routeProvider','$locationProvider'];
 
-  function config($routeProvider, $locationProvider){
-    $routeProvider
-      .when('/user/1/edit', {
-        templateUrl: 'form/form.tpl.html',
-        controller: 'FormController',
-        controllerAs: 'vm'
-      })
-      .otherwise({
-        redirectTo: '/user/1/edit'
-      });
-  }
+    function config($routeProvider, $locationProvider){
 
+      $routeProvider
+        .when('/user/1/edit', {
+          templateUrl: 'form/form.tpl.html',
+          controller: 'FormController',
+          controllerAs: 'vm'
+        })
+        .otherwise({
+          redirectTo: '/user/1/edit'
+        });
+
+    }
 })();
